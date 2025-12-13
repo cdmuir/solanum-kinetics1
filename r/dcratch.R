e982c7cb81179e79dad6c6c311b961cd2c6d47b9
cdmuir <cdmuir@wisc.edu>
2025-12-12 22:41:53 -0600

    figuring out modeling stuff

---
+++ r/dcratch.R
@@ -0,0 +1,22 @@
+p_to_a2 <- function(P, pars) {
+  pars["a_max"] + (pars["y_0"] - pars["a_max"]) * (1 + P / pars["b"]) ^ pars["a"]
+}
+pt <- function(t, pars) {
+  pars["P_f"] + (pars["P_0"] - pars["P_f"]) * exp(-pars["r"] * t)
+}
+
+pars1 <- c(P_0 = 2, P_f = 1, r = 0.005)
+pars2 <- c(y_0 = 150, a_max = 950, b = 1, a = -2)
+
+t <- seq(0, 30 * 60, 1) # 30 minutes
+P <- pt(t, pars1)
+a2 <- p_to_a2(P, pars2)
+
+plot(P, a2)
+plot(t, P, type = "l", xlab = "Time (s)", ylab = "P", main = "Function pt")
+plot(t, a2, type = "l", xlab = "P", ylab = "a2", main = "Function p_to_a2")
+
+fit <- nls(a2 ~ g_f + (g_i - g_f) * exp(-(t/tau)^lambda), start = c(g_f = 750, g_i = 950, tau = 500, lambda = 1))
+points(t, predict(fit))
+cor(a2, predict(fit))
+fit
