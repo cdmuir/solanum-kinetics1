e7ea37c33a1aa91ffccd0d4b2607f2e131ed3922
cdmuir <chrisdmuir@gmail.com>
2025-12-14 16:23:53 -0600

    blah

--- r/dcratch.R
+++ r/dcratch.R
@@ -5,6 +5,12 @@ pt <- function(t, pars) {
   pars["P_f"] + (pars["P_0"] - pars["P_f"]) * exp(-pars["r"] * t)
 }
 
+fn <- function(pars, t, y) {
+  P <- pt(t, pars[1:3])
+  a2 <- p_to_a2(P, pars[4:7])
+  sum((y - a2) ^ 2)
+}
+
 pars1 <- c(P_0 = 2, P_f = 1, r = 0.005)
 pars2 <- c(y_0 = 150, a_max = 950, b = 1, a = -2)
 
@@ -16,7 +22,10 @@ plot(P, a2)
 plot(t, P, type = "l", xlab = "Time (s)", ylab = "P", main = "Function pt")
 plot(t, a2, type = "l", xlab = "P", ylab = "a2", main = "Function p_to_a2")
 
-fit <- nls(a2 ~ g_f + (g_i - g_f) * exp(-(t/tau)^lambda), start = c(g_f = 750, g_i = 950, tau = 500, lambda = 1))
-points(t, predict(fit))
-cor(a2, predict(fit))
-fit
+fit1 <- nls(a2 ~ g_f + (g_i - g_f) * exp(-(t/tau)^lambda), start = c(g_f = 750, g_i = 950, tau = 500, lambda = 1))
+points(t, predict(fit1))
+cor(a2, predict(fit1))
+fit1
+
+fit2 <- optim(c(pars1, pars2), fn, t = t, y = a2)
+fit2
