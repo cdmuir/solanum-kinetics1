f78451e14a64884f520f34ae052ab605c76ccd5b
cdmuir <cdmuir@wisc.edu>
2026-02-10 15:28:59 -0600

    increasing iter on fits_amphi

--- r/10_compare-models.R
+++ r/10_compare-models.R
@@ -1,7 +1,15 @@
 # Compare models using LOOIC
 source("r/header.R")
+
+plan(multisession, workers = 16)
+
 fits_amphi = read_rds("objects/fits_amphi.rds") |>
   mutate(loo = map(fit, \(.x) .x$criteria$loo))
+
+converged = fits_amphi$fit |>
+  future_map_lgl(check_convergence, convergence_criteria)
+
+assert_true(all(converged))
 
 looic_table = fits_amphi$loo |>
   set_names(paste0("model", seq_along(fits_amphi$loo))) |>
