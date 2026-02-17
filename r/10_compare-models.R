dd26ecda400cb23dfdae8266353e9739b3326900
cdmuir <cdmuir@wisc.edu>
2026-02-17 08:56:09 -0600

    added tbl-comparison

--- r/10_compare-models.R
+++ r/10_compare-models.R
@@ -15,8 +15,61 @@ looic_table = fits_amphi$loo |>
   set_names(paste0("model", seq_along(fits_amphi$loo))) |>
   loo_compare()
 
-fits_amphi$fit[[1]]
-write_rds(
-  fits_amphi$fit[[as.numeric(str_extract(rownames(looic_table)[1], "\\d+"))]],
-  "objects/best_amphi_model.rds"
-)
+# Write table of model comparisons
+### THIS IS CLOSE. need to:
+### 1. add LOOIC
+### 2. add delta LOOIC
+### 3. chance explanatory var names, only use gcl and fgmax
+### 4. change to check marks
+
+fits_amphi = fits_amphi |>
+  mutate(model = paste0("model", row_number()))
+
+map2_dfr(fits_amphi$fit, fits_amphi$model, \(.fit, .name) {
+  tibble(par = .fit |>
+           as_draws_df() |>
+           select(contains("b_")) |>
+           colnames()) |>
+    mutate(par = str_remove(par, "b_")) |>
+    separate_wider_delim(par,
+                         delim = "_",
+                         names = c("response", "explanatory")) |>
+    filter(
+      str_detect(response, "^log(lambda|tau)mean$"),
+      explanatory %in% c("logfgmax", "loggcl")
+    ) |>
+    mutate(
+      response = fct_recode(response, `$\\lambda$` = "loglambdamean", `$\\tau$` = "logtaumean"),
+      explanatory = fct_recode(explanatory, `\\gcl` = "loggcl", `\\fgmax` = "logfgmax"),
+      model = .name
+    )
+  
+}) |>
+  mutate(value = TRUE) |>
+  pivot_wider(id_cols = c(response, model), names_from = explanatory) |>
+  pivot_wider(
+    id_cols = model,
+    names_from = response,
+    values_from = c(`\\gcl`, `\\fgmax`),
+    names_glue = "{response}__{.value}"
+  ) |>
+  full_join(tibble(
+    model = rownames(looic_table),
+    `$\\Delta \\text{LOOIC}$` = -2 * looic_table[, "elpd_diff"]
+  ),
+  by = "model") |>
+  mutate(across(where(is_logical), \(.x) ifelse(.x, "\\cmark", ""))) |>
+  arrange(`$\\Delta \\text{LOOIC}$`) |>
+  mutate(
+    `$\\Delta \\text{LOOIC}$` = formatC(
+      abs(`$\\Delta \\text{LOOIC}$`),
+      format = "f",
+      digits = 2
+    ),
+    mutate(across(everything(), \(.x) replace_na(.x, "")))
+  ) |>
+  write_rds("objects/tbl-comparison.rds")
+
+
+# Write best model
+write_rds(fits_amphi$fit[[as.numeric(str_extract(rownames(looic_table)[1], "\\d+"))]], "objects/best_amphi_model.rds")
