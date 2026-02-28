26f5fddea74bcb83402a85ed5c51bc6f7f68e4f9
cdmuir <cdmuir@wisc.edu>
2026-02-28 14:19:18 -0600

    added fig-mediation to results

--- r/23?_plot-mediation.R
+++ r/23?_plot-mediation.R
@@ -1,11 +1,11 @@
-# Test whether plasticity in stomatal anatomy mediates the effects of light 
+# Test whether plasticity in stomatal anatomy mediates the effects of light
 # and curve type on tau.
 
-# NOTE: We can't include phylogenetic and residual covariances in mediation 
-# because effects of lighttreatment and lightintensity are not varying at those 
+# NOTE: We can't include phylogenetic and residual covariances in mediation
+# because effects of lighttreatment and lightintensity are not varying at those
 # levels.
 
-source("r/header.R") 
+source("r/header.R")
 
 # Posterior samples of model parameters
 post = read_rds("objects/best_model.rds") |>
@@ -14,122 +14,237 @@ post = read_rds("objects/best_model.rds") |>
 
 # List of tables of direct and indirect effects for text output
 mediation = list(
-  # total effect = direct effect of sun on tau + 
+  # total effect = direct effect of sun on tau +
   #   mediated effect of sun on tau through fgmax +
   #   mediated effect of sun on tau through gcl
   lighttreatment = post |>
     mutate(
       direct_effect = b_logtaumean_lighttreatmentsun,
       mediated_effect_fgmax = b_logitfgmax_lighttreatmentsun * b_logtaumean_logitfgmax,
-      mediated_effect_gcl = b_loggcl_lighttreatmentsun * b_logtaumean_loggcl,
+      # mediated_effect_gcl = b_loggcl_lighttreatmentsun * b_logtaumean_loggcl,
+      proportion_mediated = mediated_effect_fgmax / (direct_effect + mediated_effect_fgmax),
       .keep = "none"
     ),
   
-  # total effect = direct effect of high light on tau + 
-  #   mediated effect of high light on tau through fgmax 
+  # total effect = direct effect of high light on tau +
+  #   mediated effect of high light on tau through fgmax
   # GCL did not change between measurement light intensity treatments
   lightintensity = post |>
     mutate(
       direct_effect = b_logtaumean_lightintensityhigh,
       mediated_effect_fgmax = b_logitfgmax_lightintensityhigh * b_logtaumean_logitfgmax,
+      proportion_mediated = mediated_effect_fgmax / (direct_effect + mediated_effect_fgmax),
       .keep = "none"
     ),
   
-  # total effect = direct effect of pseudohypo on tau + 
+  # total effect = direct effect of pseudohypo on tau +
   #   mediated effect of pseudohypo on tau through fgmax +
   #   mediated effect of pseudohypo on tau through gcl
   curvetype = post |>
     mutate(
       direct_effect = b_logtaumean_curve_typepseudohypo,
       mediated_effect_fgmax = b_logitfgmax_curve_typepseudohypo * b_logtaumean_logitfgmax,
-      mediated_effect_gcl = b_loggcl_curve_typepseudohypo * b_logtaumean_loggcl,
+      # mediated_effect_gcl = b_loggcl_curve_typepseudohypo * b_logtaumean_loggcl,
+      proportion_mediated = mediated_effect_fgmax / (direct_effect + mediated_effect_fgmax),
       .keep = "none"
     )
   
 ) |>
-  map(\(.x) summarize_draws(.x, estimate = median, quantile2, .args = list(probs = c(0.025, 0.975))))
+  map(\(.x) summarize_draws(
+    .x,
+    estimate = median,
+    quantile2,
+    .args = list(probs = c(0.025, 0.975))
+  ))
 
 # Plotting mediation paths as a path diagram
-df_edges = post |>
-  summarize_draws(estimate = median, quantile2, .args = list(probs = c(0.025, 0.975))) |>
+
+## Edges ----
+
+post_summary = post |>
+  summarize_draws(estimate = median, se = \(.x, ...) sd(.x), quantile2, .args = list(probs = c(0.025, 0.975))) |>
+  mutate(variable = str_replace(variable, "curve_type", "curvetype")) |>
+  separate_wider_delim(variable,
+                       delim = "_",
+                       names = c("b", "to", "from")) |>
+  select(-b) 
+
+### Growth light intensity
+df_edges_sun = post_summary |>
+  filter(to %in% c("logtaumean", "logitfgmax"), !(
+    from %in% c("Intercept", "lightintensityhigh", "curvetypepseudohypo")
+  )) |>
+  prepare_edges() 
+
+### Measurement light intensity
+df_edges_high = post_summary |>
+  filter(to %in% c("logtaumean", "logitfgmax"), !(
+    from %in% c("Intercept", "lighttreatmentsun", "curvetypepseudohypo")
+  )) |>
+  prepare_edges()
+
+### Pseudohypo curve type
+df_edges_pseudohypo = post_summary |>
+  filter(to %in% c("logtaumean", "logitfgmax"), !(
+    from %in% c("Intercept", "lightintensityhigh", "lighttreatmentsun")
+  )) |>
+  prepare_edges()
+
+## Nodes ----
+df_nodes = crossing(
+  name = c("sun treatment", "high light", "pseudohypo"),
+  x = 0.1,
+  y = 0.5,
+  explanatory = TRUE
+) |>
+  add_row(name = "$f_\\mathrm{gmax}$", x = 0.4, y = 0.75, explanatory = FALSE) |>
+  # add_row(name = "$l_\\mathrm{gc}$", x = 0.4, y = 0.25, explanatory = FALSE) |>
+  add_row(name = "$\\tau$", x = 0.8, y = 0.5, explanatory = FALSE)
+
+df_node_labels = df_nodes |>
+  crossing(
+    treatment = c("sun treatment", "high light", "pseudohypo")
+  ) |>
+  filter((name == treatment) | !explanatory) |>
+  mutate(treatment = case_when(
+    treatment == "sun treatment" ~ "Growth\nlight intensity",
+    treatment == "high light" ~ "Measurement\nlight intensity",
+    treatment == "pseudohypo" ~ "Curve type",
+    TRUE ~ treatment
+  ))
+
+df_facet_labels = df_node_labels |>
+  summarize(treatment = unique(treatment), .by = treatment) |>
+  mutate(x = -Inf, y = Inf)
+
+## Join nodes to edges ----
+
+edge_plot = join_nodes_edges(df_edges_sun, df_nodes) |>
+  full_join(
+    tribble(
+      ~from, ~to, ~pstart, ~pend,
+      "sun treatment", "$f_\\mathrm{gmax}$", 0.2, 0.8,
+      "sun treatment", "$\\tau$", 0.25, 0.9,
+      "$f_\\mathrm{gmax}$", "$\\tau$", 0.15, 0.9
+    ), by = join_by(to, from)
+  ) |>
   mutate(
-    variable = str_replace(variable, "curve_type", "curvetype")
+    xstart1 = xstart + pstart * (xend - xstart),
+    ystart1 = ystart + pstart * (yend - ystart),
+    xend1 = xstart + pend * (xend - xstart),
+    yend1 = ystart + pend * (yend - ystart),
+    treatment = "Growth\nlight intensity"
   ) |>
-  separate_wider_delim(variable, delim = "_", names = c("b", "to", "from")) |>
-  select(-b) |>
-  filter(to %in% c("logtaumean", "logitfgmax", "loggcl"), 
-         !(from %in% c("Intercept", "lightintensityhigh", "curvetypepseudohypo"))
-  ) |>
-  mutate(
-    abs_est = abs(estimate),
-    sig = as.factor(ifelse(`q2.5` > 0 | `q97.5` < 0, "sig.", "n.s.")),
-    sign = ifelse(estimate >= 0, "positive", "negative"),
-    label = sprintf("%.2f (%.2f,%.2f)", estimate, `q2.5`, `q97.5`)
-  ) |>
-  mutate(across(c(from, to), \(.x) recode(.x, 
-    logtaumean = "$\\tau$",
-    logitfgmax = "$f_\\text{gmax}$",
-    loggcl = "$l_\\text{gc}$",
-    lighttreatmentsun = "sun treatment"
-  )))
+  bind_rows(
+    join_nodes_edges(df_edges_high, df_nodes) |>
+      full_join(
+        tribble(
+          ~from, ~to, ~pstart, ~pend,
+          "high light", "$f_\\mathrm{gmax}$", 0.2, 0.8,
+          "high light", "$\\tau$", 0.2, 0.9,
+          "$f_\\mathrm{gmax}$", "$\\tau$", 0.15, 0.9
+        ), by = join_by(to, from)
+      ) |>
+      mutate(
+        xstart1 = xstart + pstart * (xend - xstart),
+        ystart1 = ystart + pstart * (yend - ystart),
+        xend1 = xstart + pend * (xend - xstart),
+        yend1 = ystart + pend * (yend - ystart),
+        treatment = "Measurement\nlight intensity"
+      )
+    ) |>
+  bind_rows(
+    join_nodes_edges(df_edges_pseudohypo, df_nodes) |>
+      full_join(
+        tribble(
+          ~from, ~to, ~pstart, ~pend,
+          "pseudohypo", "$f_\\mathrm{gmax}$", 0.2, 0.8,
+          "pseudohypo", "$\\tau$", 0.2, 0.9,
+          "$f_\\mathrm{gmax}$", "$\\tau$", 0.15, 0.9
+        ), by = join_by(to, from)
+      ) |>
+      mutate(
+        xstart1 = xstart + pstart * (xend - xstart),
+        ystart1 = ystart + pstart * (yend - ystart),
+        xend1 = xstart + pend * (xend - xstart),
+        yend1 = ystart + pend * (yend - ystart),
+        treatment = "Curve type")
+    ) |>
+  mutate(angle = 180 / pi * atan2(yend - ystart, xend - xstart))
 
-df_nodes = tribble(
-  ~name, ~x, ~y,
-  "sun treatment", 0.1, 0.5,
-  "$f_\\text{gmax}$", 0.4, 0.75,
-  "$l_\\text{gc}$", 0.4, 0.25,
-  "$\\tau$", 0.8, 0.5
-)
-
-# join node coordinates to edges for plotting
-edge_plot = df_edges |>
-  left_join(df_nodes, by = c("from" = "name")) |>
-  rename(xstart = x, ystart = y) |>
-  left_join(df_nodes, by = c("to" = "name")) |>
-  rename(xend = x, yend = y)
-
-# Plot
-p <- ggplot() +
-  # arrows (use geom_curve for curved mediated paths)
+## Plot ----
+p <- ggplot(data = edge_plot) +
+  facet_grid(rows = vars(treatment), switch = "y") +
   geom_curve(
-    data = edge_plot |> filter(!(from == "sun treatment" & to == "$\\tau$")), # mediated edges curved
-    aes(x = xstart, y = ystart, xend = xend, yend = yend,
-        color = sign, size = abs_est, linetype = sig),
-    curvature = 0.25,
-    arrow = arrow(length = unit(0.18, "inches"), type = "closed"),
+    aes(
+      x = xstart1,
+      y = ystart1,
+      xend = xend1,
+      yend = yend1,
+      color = sign,
+      linewidth = effect_size,
+      linetype = sig
+    ),
+    curvature = 0,
+    arrow = arrow(length = unit(0.1, "inches"), type = "closed"),
     lineend = "round",
     show.legend = TRUE
   ) +
-  # direct path as a straight line
-  geom_segment(
-    data = edge_plot |> filter(from == "sun treatment" & to == "$\\tau$"),
-    aes(x = xstart, y = ystart, xend = xend, yend = yend,
-        color = sign, size = abs_est, linetype = sig),
-    arrow = arrow(length = unit(0.18, "inches"), type = "closed"),
-    lineend = "round"
+  geom_label(
+    data = df_node_labels,
+    mapping = aes(x = x, y = y, label = name),
+    fill = "white",
+    fontface = 2
   ) +
-  # node points
-  geom_point(data = df_nodes, aes(x = x, y = y), size = 8, shape = 21, fill = "white") +
-  geom_text(data = df_nodes, aes(x = x, y = y, label = name), size = 4, fontface = "bold") +
-  # effect labels halfway along each edge
-  geom_label_repel(
+  geom_text(
+    data = df_facet_labels,
+    mapping = aes(x = x, y = y, label = treatment),
+    fontface = 3,
+    hjust = 0,
+    vjust = 1
+  ) +
+  geom_text(
     data = edge_plot,
-    aes(x = (xstart + xend)/2, y = (ystart + yend)/2, label = label),
-    size = 3,
-    nudge_y = 0.03,
-    segment.size = 0
+    aes(
+      x = (xstart + xend) / 2,
+      y = (ystart + yend) / 2,
+      label = label,
+      angle = angle
+    ),
+    size = 3, vjust = -2
   ) +
-  # scales
-  scale_size_continuous(range = c(0.5, 2.5), guide = "none") +
-  scale_color_manual(values = c(positive = "steelblue", negative = "firebrick")) +
-  scale_linetype_manual(values = c(`sig.` = "solid", `n.s.` = "dashed"), guide = guide_legend("significance")) +
-  theme_minimal() +
+  scale_x_continuous(limits = c(0, 1)) +
+  scale_y_continuous(limits = c(0.4, 0.85)) +
+  scale_color_manual(values = c(
+    positive = "steelblue",
+    negative = "firebrick",
+    guide = guide_legend(override.aes = list(linewidth = 100))
+  )) +
+  scale_linewidth_continuous(
+    breaks = c(5, 15, 45),
+    name = "effect size") +
+  scale_linetype_discrete(guide = if (length(unique(edge_plot$sig)) > 1) {
+    guide_legend()
+  } else {
+    "none"
+  }
+  ) +
+  theme_void() +
   theme(
-    axis.title = element_blank(),
-    axis.text = element_blank(),
-    axis.ticks = element_blank()
-  ) +
-  coord_fixed()
-
+    legend.key.spacing.y = unit(0.5, "cm"),
+    legend.key.width = unit(2, "cm"),
+    strip.text = element_blank())
 print(p)
-    
+
+tikz(
+  "figures/mediation.tex",
+  standAlone = TRUE,
+  width = 6,
+  height = 6
+)
+print(p)
+dev.off()
+
+system("cd figures; pdflatex mediation.tex; rm mediation.aux mediation.log")
+
+write_rds(mediation, "objects/mediation.rds")
